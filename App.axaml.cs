890558037f77834744206144d23b951061df795f
Bryan <bryansampica@hotmail.com>
2023-10-07 12:08:50 -0400

    Setup project for Raspberry pi, with a MainView and MainWindow

--- App.axaml.cs
+++ App.axaml.cs
@@ -16,12 +16,9 @@ public partial class App : Application
     public override void OnFrameworkInitializationCompleted()
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-        {
-            desktop.MainWindow = new MainWindow
-            {
-                DataContext = new MainWindowViewModel(),
-            };
-        }
+            desktop.MainWindow = new MainWindow();
+        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleView)
+            singleView.MainView = new MainView();
 
         base.OnFrameworkInitializationCompleted();
     }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using Avalonia;
+using Avalonia.LinuxFramebuffer.Output;
 using Avalonia.ReactiveUI;
 using System;
+using System.Linq;
 
 namespace BlackWater;
 
@@ -10,8 +12,25 @@ class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static int Main(string[] args)
+    {
+        var builder = BuildAvaloniaApp();
+        if (args.Contains("--drm"))
+        {
+            return builder.StartLinuxDrm(args, null, 1);
+        }
+        if (args.Contains("--fb"))
+        {
+            return builder.StartLinuxFbDev(args);
+        }
+        if (args.Contains("--direct"))
+        {
+            return builder.StartLinuxDirect(args, new DrmOutput());
+        }
+
+        return builder.StartWithClassicDesktopLifetime(args);
+    }
+       
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
